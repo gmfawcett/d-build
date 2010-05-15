469e15a68ade175a37ba47f0e69e9d3d8aa413d9
Graham Fawcett <graham.fawcett@gmail.com>
2010-05-14 22:32:24 -0400

    playing with dcrypt.
    dcrypt almost imports properly. I needed to patch it to make it work
    with the latest D2.

--- DEPS
+++ DEPS
@@ -7,3 +7,7 @@ git init && git pull git://github.com/gmfawcett/d-play-libcurl.git && git checko
 
 # dranges. note, this will fail on Windows (though not if using cygwin/mingw):
 svn checkout http://svn.dsource.org/projects/dranges/trunk && mv trunk dranges
+
+# dcrypt. Note, I had to patch r36 after the clone: see dcrypt.patch.
+hg clone -r36 http://hg.dsource.org/projects/dcrypt && cd dcrypt && hg import --no-commit ../../../dcrypt.patch
+
--- test.d
+++ test.d
@@ -2,6 +2,7 @@ import std.algorithm	: array;
 import std.stdio	: writefln;
 import dranges.range2	: asSet;
 import fawcett.curl	: Curl;
+import dcrypt.crypto.hashes.SHA256 : SHA256;
 
 void main() {
   // curl testing
@@ -11,4 +12,10 @@ void main() {
 
   // dranges testing.
   writefln("dranges test: %s", array(asSet([1,2,3,4,5,4,3,2,1])));
+
+  // dcrypt testing.
+  SHA256 h = new SHA256();
+  h.update("hello");
+  writefln("dcrypt test:  %s", h.hexDigest());
+  writefln("should print: %s", "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
 }
